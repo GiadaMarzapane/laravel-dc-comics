7f9c025af7ea5ca508a5d60220107a956110019d
GiadaMarzapane <giada.ortesta@gmail.com>
2023-03-17 16:12:25 +0100

    Update textarea input

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -17,19 +17,44 @@
                         @csrf
                         <div class="mb-3">
                             <label for="title" class="form-label">Titolo*:</label>
-                            <input required type="text" name="title" class="form-control" id="title" placeholder="Inserisci titolo">
+                            <input required
+                            type="text"
+                            name="title"
+                            class="form-control"
+                            id="title"
+                            placeholder="Inserisci titolo"
+                            value="{{ old('title') }}">
                         </div>
                         <div class="mb-3">
                             <label for="series" class="form-label">Serie*:</label>
-                            <input required type="text" name="series" class="form-control" id="series" placeholder="Inserisci la serie">
+                            <input required
+                            type="text"
+                            name="series"
+                            class="form-control"
+                            id="series"
+                            placeholder="Inserisci la serie"
+                            value="{{ old('series') }}">
                         </div>
                         <div class="mb-3">
                             <label for="price" class="form-label">Prezzo*:</label>
-                            <input required type="number" min="1" step="0.01" name="price" class="form-control" id="price" placeholder="Inserisci il prezzo">
+                            <input required
+                            type="number"
+                            min="1"
+                            step="0.01"
+                            name="price"
+                            class="form-control"
+                            id="price"
+                            placeholder="Inserisci il prezzo"
+                            value="{{ old('price') }}">
                         </div>
                         <div class="mb-3">
                             <label for="description" class="form-label">Descrizione:</label>
-                            <textarea type="text" name="description" class="form-control" id="description" placeholder="Inserisci descrizione comic"></textarea>
+                            <textarea type="text"
+                            name="description"
+                            class="form-control"
+                            id="description"
+                            rows="6"
+                            placeholder="Inserisci descrizione comic">{{ old('description') }}</textarea>
                         </div>
                         <button type="submit" class="btn btn-primary">Inserisci Comic</button>
                     </form>
